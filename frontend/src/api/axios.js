8b3e6e1f79e10e42f1ceb5774cafc6d7225f9d0f
Abhinash02 <exampaperguide.in@gmail.com>
2026-05-07 17:52:48 +0530

    feat: update API configuration and add Vercel rewrites for deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -35,18 +35,39 @@
 // export default api;
 
 
+// import axios from "axios";
+// import { getToken } from "../utils/storage";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:4000/api",
+// });
+
+
+// api.interceptors.request.use((config) => {
+//   const token = getToken();
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default api;
+
+
 import axios from "axios";
 import { getToken } from "../utils/storage";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 api.interceptors.request.use((config) => {
   const token = getToken();
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
